bc99114c47465c94477159d2ba1608652a6dfce4
Ivan Dolezal <ivan.dolezal@gmail.com>
2014-10-24 22:27:00 +0200

    UPDATE - Promise, jquery async data load

--- components/Button.jsx
+++ components/Button.jsx
@@ -23,6 +23,7 @@ var Button = React.createClass({
         return {
           pending:false,
           active : false,
+          icon:this.props.icon
         };
     },
     /**
@@ -30,24 +31,44 @@ var Button = React.createClass({
      */
     componentDidMount:function(){
         console.log("Button component was mount to dom");
-        Store.addChangeListener(this.onChange);
+        Store.addListener(Constants.CHANGE_EVENT,this.onChange);
+        Store.addListener(Constants.LOADING_EVENT,this.onLoading);
+        Store.addListener(Constants.LOADING_DONE_EVENT,this.onLoadingDone);
     },
     /**
      * Overriding React Class lifecycle method
      */
     componentWillUnmount:function(){
-        Store.removeChangeListener(this.onChange);
+        // cleanup
+        Store.removeListener(Constants.CHANGE_EVENT,this.onChange);
+        Store.removeListener(Constants.LOADING_EVENT,this.onLoading);
+        Store.removeListenner(Constants.LOADING_DONE_EVENT,this.onLoadingDone)
+    },
+    iAmOrigin:function(){
+       var result = Store.getActivebutton()== this.props.buttonType;
+       return result;
     },
     /**
      *  Handler of the change caught by listenning
      */
     onChange:function(){
-        if(Store.getActivebutton()==this.props.buttonType) {
+        if(this.iAmOrigin()) {
             this.setState({active:true});
         } else {
             this.setState({active:false});
         }
         console.log("Button " + this.props.buttonType + " state changed to " + this.state.active);
+    },
+    onLoading:function(){
+            var pd  =this.iAmOrigin();
+            if(pd) {
+                this.setState({icon:"fa-spinner fa-spin"}) // we show loading icon
+            }
+            this.setState({pending:pd})
+    },
+    onLoadingDone:function(){
+        this.setState({pending:false});
+        this.setState({icon:this.props.icon}) // we set icon back to the one set by props
     },
     /**
      * Handler of click event triggered on button
@@ -56,14 +77,14 @@ var Button = React.createClass({
         AppDispatcher.handleViewAction({actionType: Constants.CHANGE_EVENT,
             buttonType:this.props.buttonType}
         );
-        console.log("Button" + this.props.buttonType + " clicked");
+        console.log("Button " + this.props.buttonType + " clicked");
     },
     /**
      * Renders component into view
      * @returns {XML}
      */
     render: function() {
-        return(<Btn bsStyle="primary" bsSize="large" active={this.state.active} onClick={this.onClick}>{this.props.icon!=undefined ? <Icon icon={this.props.icon}/> : ''}{this.props.text}</Btn>)
+        return(<Btn bsStyle="primary" bsSize="large" active={this.state.active} disabled={this.state.pending} onClick={this.onClick}>{this.props.icon!=undefined ? <Icon icon={this.state.icon}/> : ''}{this.props.text}</Btn>)
     }
 });
 
