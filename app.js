ed7c3f662ccf890ecdb70484ae57b6a0c07140f2
Ivan Doležal <ivan.dolezal@gmail.com>
2014-10-26 15:54:33 +0100

    UPDATE - new files added to repo

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, '/views/'));
 app.set('view engine', 'jsx');
-app.engine('jsx', require('express-react-views').createEngine({jsx: {harmony: true},beautify:true}));
+app.engine('jsx', require('express-react-views').createEngine({jsx: {harmony: true,extension:".js"},beautify:true}));
 
 
 app.use(logger('dev'));
